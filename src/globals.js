35fb808008e8911fd3c8fa2a8a0875b8fd3d439b
wushufen <shufen.wu@shengyc.com>
2025-07-02 14:52:15 +0800

    pref globals

--- src/globals.js
+++ src/globals.js
@@ -1,28 +1,27 @@
-export const globalThis = Function('return this')()
-export const window = globalThis
 export const global = globalThis
+export const window = global
 
-export const Object = globalThis.Object
-export const Array = globalThis.Array
-export const String = globalThis.String
-export const Number = globalThis.Number
-export const Boolean = globalThis.Boolean
-export const Symbol = globalThis.Symbol
+export const Object = global.Object
+export const Array = global.Array
+export const String = global.String
+export const Number = global.Number
+export const Boolean = global.Boolean
+export const Symbol = global.Symbol
 
-export const Map = globalThis.Map
-export const Set = globalThis.Set
-export const WeakMap = globalThis.WeakMap
-export const WeakSet = globalThis.WeakSet
+export const Map = global.Map
+export const Set = global.Set
+export const WeakMap = global.WeakMap
+export const WeakSet = global.WeakSet
 
-export const Reflect = globalThis.Reflect
-export const Proxy = globalThis.Proxy
-export const Promise = globalThis.Promise
+export const Reflect = global.Reflect
+export const Proxy = global.Proxy
+export const Promise = global.Promise
 
-export const Node = globalThis.Node
-export const Element = globalThis.Element
-export const HTMLElement = globalThis.HTMLElement
-export const SVGElement = globalThis.SVGElement
-export const Text = globalThis.Text
+export const Node = global.Node
+export const Element = global.Element
+export const HTMLElement = global.HTMLElement
+export const SVGElement = global.SVGElement
+export const Text = global.Text
 
-export const document = globalThis.document
-export const customElements = globalThis.customElements
+export const document = global.document
+export const customElements = global.customElements
